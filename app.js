a8044a256803c7012a09e472c33cad4c46d6b244
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-07-25 18:09:26 -0400

    function statement vs function expression

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+// let ifVariable = if (true) {
+
+// }
+
+let var1;
+
+console.log(var1);
+
+sayHello(); //it's ok
+sayHelloExpression(); //not ok!!!
+
+function sayHello () {  //function statement
+    return "Hello!";
+}
+
+let sayHelloExpression = function sayHelloExpression () { //function expression
+    return "Hello!";
+}
+
