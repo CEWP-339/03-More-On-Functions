f7623a67a05eb1397336cd8ce4aa76faca464f9d
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-07-25 19:11:36 -0400

    use arrow functions

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = 'ROCK';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
-const getPlayerChoice = function () {
-    const selection = prompt(`Please enter your selection: ${ROCK}, ${PAPER}, ${SCISSORS}`, '');
+const getPlayerChoice = () => {
+    const selection = prompt(`Please enter your selection: ${ROCK}, ${PAPER}, ${SCISSORS}`, '').toUpperCase();
     if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
         alert(`Invalid Choice! We choose ${DEFAULT_USER_CHOICE} for you!`);
         return DEFAULT_USER_CHOICE;
@@ -14,8 +17,34 @@ const getPlayerChoice = function () {
     return selection;
 }
 
+const getComputerChoice = () => {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+const getWinner = (pChoice, cChoice) => {
+    if (pChoice === cChoice) {
+        return RESULT_DRAW;
+    } else if (pChoice === SCISSORS && cChoice === ROCK ||
+               pChoice === PAPER && cChoice === SCISSORS ||
+               pChoice === ROCK && cChoice === PAPER) {
+        return RESULT_COMPUTER_WINS;
+    } else {
+        return RESULT_PLAYER_WINS;
+    }
+}
+
 startGameBtn.addEventListener('click', function () {
     console.log('Game is starting....');
     const playerSelection = getPlayerChoice();
     console.log(playerSelection);
+    const computerSelection = getComputerChoice();
+    const winner = getWinner(playerSelection, computerSelection);
+    console.log(winner);
 });
