224dfcb7d2677fd2515a256b2eda199960c048a9
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-07-25 18:48:40 -0400

    get user choice

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const person = {
-//     name: "KS",
-//     greet: function greet () {
-//         console.log("hello!");
-//     }
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = 'ROCK';
 
-// console.log(person.name); //KS
-// person.greet();
+const getPlayerChoice = function () {
+    const selection = prompt(`Please enter your selection: ${ROCK}, ${PAPER}, ${SCISSORS}`, '');
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert(`Invalid Choice! We choose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+}
 
 startGameBtn.addEventListener('click', function () {
     console.log('Game is starting....');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 });
